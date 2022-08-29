3a12488d15a4e7d782f76ac54baec61d3e781218
DavidPrades <davidprades8@gmail.com>
2022-08-29 10:42:52 +0200

    Finalizada Aplicación de Gifs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    GifsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
